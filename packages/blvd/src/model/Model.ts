cc76ce361b202c7ff62772554f5af6740f7936ef
Kyle <kyle@vulpine.me>
2017-03-16 09:53:40 -0400

    fix(Model): allow new items to have predefined ID (for fetched items)

--- packages/blvd/src/model/Model.ts
+++ packages/blvd/src/model/Model.ts
@@ -4,6 +4,10 @@ import Context from '../context/Context'
 import PropertyType from '../propertyTypes/PropertyType'
 import PropertyTypes from '../propertyTypes/PropertyTypes'
 
+interface ObjectThatMightHaveId {
+  id?: string
+}
+
 /**
  * The Model class represents a Model in a traffic application. A Model is a
  * class upon which Items are based - essentially, a traffic app has a bunch of
@@ -23,11 +27,11 @@ class Model {
     id: [PropertyTypes.string(), PropertyTypes.unique('id')]
   }
 
-  constructor(public context: Context, public properties: object = {}) {
+  constructor(public context: Context, public properties: ObjectThatMightHaveId = {}) {
     // First, generate a unique ID for this Item
-    this.generateId()
+    (!properties.id ? this.generateId() : Promise.resolve(properties.id))
       .then((id: string) => {
-        // Then, add the ID to the properties of the item
+        // Then, add the ID to the properties of the item if it doesn't have an ID
         this.properties = { ...this.properties, id }
       })
       .then(() => this.checkPropertyTypes(this.constructor.prototype.properties, this.properties))
@@ -45,7 +49,7 @@ class Model {
   private async checkPropertyTypes(propertyTypes: object, properties: object): Promise<boolean> {
     // Listen, I know you wanna read this function.
     // I do too.
-    const checkResults: boolean[] = await Promise.all(Object.keys(this.properties).map((property): Promise<boolean> => {
+    const checkResults: boolean[] = await Promise.all(Object.keys(this.properties).map((property: string): Promise<boolean> => {
       if (typeof propertyTypes[property] === 'function') {
         return new Promise((resolve: Function, reject: Function) => {
           const check: PropertyType = propertyTypes[property]
@@ -53,11 +57,11 @@ class Model {
         })
       } else if (Array.isArray(propertyTypes[property])) {
         const checks: PropertyType[] = propertyTypes[property]
-        return Promise.all(checks.map((check: Function): Promise<boolean> => new Promise((resolve: Function, reject: Function) => {
-          resolve(check(this.properties[property], this, this.constructor.prototype, this.context))
-        }))).then((results: boolean[]) => results.reduce(((prev: boolean, curr: boolean) => (prev === true && curr === true)), true))
+        return Promise.all(checks.map((check: Function): Promise<boolean> =>
+          Promise.resolve(check(this.properties[property], this, this.constructor.prototype, this.context))
+        )).then((results: boolean[]) => results.reduce(((prev: boolean, curr: boolean) => (prev === true && curr === true)), true))
       }
-      return new Promise((resolve: Function, reject: Function) => resolve(false))
+      return Promise.resolve(false)
     }))
     return checkResults.reduce(((prev: boolean, curr: boolean) => (prev === true && curr === true)), true)
   }
