3435650b6623b74829b90c31becc03634d12bd66
Kyle <kyle@vulpine.me>
2017-04-05 14:37:36 -0400

    feat(blvd-server:Server): update server to use websockets too

--- packages/blvd-server/src/Server.ts
+++ packages/blvd-server/src/Server.ts
@@ -1,30 +1,54 @@
 import micro from 'micro' // TODO: Consider Node 4 support? Micro only supports 6+ but Express sucks
 import { IncomingMessage, ServerResponse } from 'http'
 import { Client } from 'blvd-client'
+import { Server as WSServer } from 'ws'
+import { Server as HTTPServer } from 'http'
+
+interface ServerOptions {
+  port: number
+}
 
 class Server {
   // The compilePath stores where the last compiled version of the client is stored.
   protected compilePath: string
 
   // This is the backing micro server.
-  private server
+  private server: HTTPServer
 
-  constructor (private client: Client) {
-    this.server = micro(this.handle) // > express in 2017
+  // This is the websocket server.
+  private wsServer: WSServer
+
+  constructor (
+    private client: Client,
+    private options: ServerOptions = { port: 8080 }
+  ) {
+    this.server = micro(this.handleHttpRequest) // > express in 2017
+    const server = this.server
+    this.wsServer = new WSServer({ server })
+    this.wsServer.on('connection', this.handleWsRequest)
     this.setCompilePath()
   }
 
   // This starts the server.
-  public start (port: (number | string) = 8080): void {
-    this.server.listen(port)
+  public start (): void {
+    this.server.listen(this.options.port)
   }
 
-  // The handle function is passed the request object and the response object, and
-  // expected to do something with it. Whatever it does is up to you. By default,
-  // it takes the compiled path and wraps the contents of the file in a <script>
-  // tag in a barebones html file. Very simple.
-  protected async handle (req: IncomingMessage, res: ServerResponse): Promise<string> {
+  // The handleHttpRequest function is passed the request object and the response object, and
+  // expected to do something with it. Whatever it does is up to you. By default, it takes
+  // the compiled path and wraps the contents of the file in a <script> tag in a barebones
+  // html file. If you were using some sort of server rendering framework, you might instead
+  // override this function to utilize such a framework. Better yet, you'd use some sort of
+  // adapter which already did this for you!
+  protected async handleHttpRequest (req: IncomingMessage, res: ServerResponse): Promise<string> {
     return await Promise.resolve('NOT YET IMPLEMENTED')
+  }
+
+  // The handleWsRequest handles websocket requests, much like the handleHttpRequest handles
+  // http requests. However, it's generally not a good idea to override this. I can't think of
+  // a possible use case for it yet, but hey, you're the programmer here, not me.
+  protected async handleWsRequest (): Promise<void> {
+    // NOT YET IMPLEMENTED
   }
 
   // We run this to set the compile path, since client.compile() is async. Hot swap
