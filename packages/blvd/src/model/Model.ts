4c00438c4543a81a8b7c8118692e58caaab93b9d
Kyle <kyle@vulpine.me>
2017-05-08 15:26:32 -0400

    docs(blvd:Model): some more docs on the model

--- packages/blvd/src/model/Model.ts
+++ packages/blvd/src/model/Model.ts
@@ -21,8 +21,10 @@ abstract class Model {
     id: [PropertyTypes.string]
   }
 
-  // TODO: Implement public static async getByIndex(index: string, value: any)
   // TODO: Implement public static async getById(id: string)
+  // This should be implemented first, before we start to implement indexes. I also need to outline how DB adapters are gonna work.
+
+  // TODO: Implement public static async getByIndex(index: string, value: any)
 
   constructor(public properties: ObjectThatMightHaveId = {}) {
     // First, generate a unique ID for this Item
@@ -32,17 +34,9 @@ abstract class Model {
         this.properties = { ...this.properties, id }
       })
       .then(() => this.checkPropertyTypes(this.constructor.prototype.propertyTypes, this.properties))
-      .then(
-        // Then, check the properties of the item match up with the propertyTypes provided
-        () => {
-          console.log('Successfully created new model! Yay!')
-        },
-        () => {
-          console.log('Uh-oh! Something broke when checking the property types!')
-        }
-      )
   }
 
+  // TODO: I need to clarify in my mind how indexes work...
   public getIndex(): string {
     if (typeof this.properties.id === 'string') {
       return this.properties.id
@@ -61,27 +55,38 @@ abstract class Model {
   // just pass the original role object for clearer syntax, e.g.,
   // @this.requireRole('GOOD_BOY') === @requireRole(this.roles.GOOD_BOY) === @requireRole(DogModel.getById(this.id).roles.GOOD_BOY)
 
+  // TODO: Consider swapping out home grown property types for using React `prop-types`?
   private async checkPropertyTypes(propertyTypes: object, properties: object): Promise<Result> {
+    // First, we create an array of results by creating an array of promises (one per property) and waiting on all of them.
     const checkResults: Result[] = await Promise.all(Object.keys(this.properties).map((property: string): Promise<Result> => {
       if (typeof propertyTypes[property] === 'function') {
+        // If the property type of the property we're checking is a function, we run the function and return the result.
         return new Promise((resolve: Function, reject: Function) => {
           const check: PropertyType = propertyTypes[property]
           resolve(check(this.properties[property], this, this.constructor.prototype))
         })
       } else if (Array.isArray(propertyTypes[property])) {
+        // If the property type of the property we're checking is an array, we assume it is an array of functions and run each function
+        // before reducing all these results into one result.
         const checks: PropertyType[] = propertyTypes[property]
         return Promise.all(checks.map((check: Function): Promise<Result> =>
           Promise.resolve(check(this.properties[property], this, this.constructor.prototype))
         )).then((results: Result[]) => results.reduce(reduceResults, { status: Status.SUCCESS }))
       }
+      // If the property type is neither a function nor an array of functions, we return a failing status because you gave a bad property
+      // type.
       return Promise.resolve({
         status: Status.FAILURE,
         error: 'One of the provided property types was not a function or array of functions.'
       })
     }))
+
+    // We then reduce all the results into a single result object.
     return checkResults.reduce(reduceResults, { status: Status.SUCCESS })
   }
 
+  // Really simple way to generate an id for an object.
+  // TODO: This should probably be tossed to a server.
   private async generateId(): Promise<string> {
     const r = await randomBytes(32)
     return r.toString('hex')
