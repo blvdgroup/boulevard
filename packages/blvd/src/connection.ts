68c551af6d48d3379699ff84bcf8ba84d32433ea
Kyle <kyle@vulpine.me>
2017-05-12 10:10:51 -0400

    feat(blvd:connection): implement connection API
    parsing raw connections into ConnectionLayer objects is still on the todo list, but the Connection
    class takes it a step higher by adding shared properties between connections and maybe more later

--- packages/blvd/src/connection.ts
+++ packages/blvd/src/connection.ts
@@ -1,6 +1,115 @@
 import { EventEmitter2 as EE } from 'eventemitter2'
+import { randomBytes } from 'crypto-promise'
+
+import { Result, Status } from 'blvd-utils'
+
+import { getContextType, ContextType } from './context'
 
 const connectionPasser = new EE()
+
+type EventHandler = (event: string, callback: (...args: any[]) => any) => any
+
+// The ConnectionLayer is the shared layer through which both types of connection are parsed through. Although a raw client connection and
+// a raw server connection look different, we construct them to have the same API (outlined here, very similar to an EventEmitter) and then
+// wrap _that_ with the Connection class for more advanced use (sharing properties, etc).
+//
+// The important thing to note about the connection layer is that although it may look the same as an EventEmitter it is constructed so that
+// running an emit call on the client should ONLY trigger event handlers on the server, and vice versa.
+interface ConnectionLayer {
+  on: EventHandler,
+  once: EventHandler,
+  emit: (event: string, ...args: any[]) => any
+}
+
+class Connection {
+  private properties: any
+  private contextType: ContextType = getContextType()
+
+  constructor (private layer: ConnectionLayer) {
+    this.layer.on('generate-id', () => {
+      if (this.contextType === ContextType.SERVER) {
+        this.generateId()
+      }
+    })
+
+    this.layer.on('assign', (property: string, value: any) => {
+      this.properties[property] = value
+      this.layer.emit(`assignResponse-${property}`, { status: Status.SUCCESS })
+    })
+
+    this.layer.on('request', (property: string) => {
+      const prop = this.properties[property]
+      if (prop) {
+        this.layer.emit(`response-${property}`, { status: Status.SUCCESS }, prop)
+      } else {
+        this.layer.emit(`response-${property}`, { status: Status.FAILURE, error: 'Property does not exist over here.' }, null)
+      }
+    })
+  }
+
+  public async getId(): Promise<string> {
+    // let's ask the other end of this connection if they know the id
+    const [ result, id ] = await this.ask('id')
+    if (result.status === Status.SUCCESS) return id
+
+    // ok... well it looks like neither of us knows the id of this connection let's make one
+    if (this.contextType === ContextType.CLIENT) {
+      // we're a client we'll let the server handle generating a connection
+      return this.waitForId()
+    } else if (this.contextType === ContextType.SERVER) {
+      // we're a server let's make a gosh darn id
+      return this.generateId()
+    }
+
+    throw new Error('Tried to get the ID of our connection, but somehow managed to break everything instead.')
+  }
+
+  private async generateId(): Promise<string> {
+    // before we do any of this... do we already have an id?
+    // just in case, let's check - async stuff can get messy sometimes
+    if (this.properties.id) return this.properties.id
+
+    // ok we don't let's make one
+    const id = (await randomBytes(32)).toString('hex')
+    await this.assign('id', id)
+    this.layer.emit('id', id)
+    return id
+  }
+
+  private async waitForId(): Promise<string> {
+    this.layer.emit('generate-id')
+
+    // Is there, like, a better way to write promises where I can skip this line?
+    // Or just make it ((resolve, reject) => {}) like I used to?
+    // Oh well, Promise constructor is an anti-pattern anyway or something.
+    return new Promise((resolve: (...args: any[]) => any, reject: (...args: any[]) => any) => {
+      this.layer.once('id', (id: string) => { resolve(id) })
+    })
+  }
+
+  private async ask(property: string): Promise<[Result, any]> {
+    // check if we already know the property
+    if (this.properties[property]) return this.properties[property]
+
+    // ask for the property
+    this.layer.emit(`request`, property)
+
+    return new Promise((resolve: (...args: any[]) => any, reject: (...args: any[]) => any) => {
+      this.layer.once(`response-${property}`, (result: Result, resp: any) => { resolve([result, resp]) })
+    })
+  }
+
+  private async assign(property: string, value: any): Promise<Result> {
+    this.layer.emit(`assign`, property, value)
+
+    return new Promise((resolve: (...args: any[]) => any, reject: (...args: any[]) => any) => {
+      this.layer.once(`assignResponse-${property}`, (result: Result) => { resolve(result) })
+    })
+  }
+}
+
+// In both of the below handlers, we take the raw connection object from either the blvd client or server and wrap it in a ConnectionLayer,
+// before passing the ConnectionLayer onto a new Connection object.
 
 connectionPasser.on('connection-client-raw', (c: any): void => {
   // Parse connection from a client object into a unified API
@@ -29,6 +138,6 @@ export default {
     })
   },
   emit: (event: string, ...args: any[]) => connectionPasser.emit(event, ...args),
-  on: (event: string, callback: () => any) => connectionPasser.on(event, callback),
-  once: (event: string, callback: () => any) => connectionPasser.on(event, callback)
+  on: (event: string, callback: (...args: any[]) => any) => connectionPasser.on(event, callback),
+  once: (event: string, callback: (...args: any[]) => any) => connectionPasser.on(event, callback)
 }
--- packages/blvd/src/context.ts
+++ packages/blvd/src/context.ts
@@ -25,7 +25,7 @@ const determineId = async (): Promise<string> => {
 declare var process
 declare var window
 
-export const getContext = async (): Promise<Context> => {
+export const getContextType = (): ContextType => {
   // First we're gonna try to find if we're a client or server.
 
   // The first and simplest way is checking the BLVD_CONTEXT environment variable.
@@ -33,8 +33,8 @@ export const getContext = async (): Promise<Context> => {
   if (process && process.env) {
     // Ok, there's a global process object. Awesome. What's it say about the context?
     switch (process.env.BLVD_CONTEXT) {
-      case 'client': return { type: ContextType.CLIENT, id: await determineId() }
-      case 'server': return { type: ContextType.SERVER }
+      case 'client': return ContextType.CLIENT
+      case 'server': return ContextType.SERVER
       default: break // If BLVD_CONTEXT isn't defined, just skip to the next attempt to try to find the context.
     }
     // (We could claim we're on a server if the process variable exists and doesn't define process.env.BLVD_CONTEXT, but this might break
@@ -44,5 +44,7 @@ export const getContext = async (): Promise<Context> => {
 
   // If the above doesn't work, we assume we're a client if the window object exists.
   // This isn't foolproof if some dumb dumb puts a global window object on a nodejs server, but it's better than nothing.
-  return window ? { type: ContextType.CLIENT, id: await determineId() } : { type: ContextType.SERVER }
+  return window ? ContextType.CLIENT : ContextType.SERVER
 }
+
+export const getContext = async (): Promise<Context> => ({ type: getContextType(), id: await determineId() })
