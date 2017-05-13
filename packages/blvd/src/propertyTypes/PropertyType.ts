cda16e155efc7e1a6876f79250f7c7998ee7bd80
Resi Respati <resir014@gmail.com>
2017-05-13 17:05:38 +0700

    fix(blvd:PropertyType): remove unnecessary generic on the model parameter
    Generics are removed from ModelConstructor as of 4b90060a, but we forgot to remove it here.

--- packages/blvd/src/propertyTypes/PropertyType.ts
+++ packages/blvd/src/propertyTypes/PropertyType.ts
@@ -2,7 +2,7 @@ import Model from '../model/Model'
 import ModelConstructor from '../model/ModelConstructor'
 
 interface PropertyType {
-  <M extends Model>(property: any, item: Model, model: ModelConstructor<M>): (boolean | Promise<boolean>)
+  <M extends Model>(property: any, item: Model, model: ModelConstructor): (boolean | Promise<boolean>)
 }
 
 export default PropertyType
