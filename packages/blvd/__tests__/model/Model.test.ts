85bea314597aa6276ba9ba47f0acf9c69b70c4df
Resi Respati <resir014@gmail.com>
2017-05-23 18:09:03 +0700

    test(blvd): more work on Model tests

--- packages/blvd/__tests__/model/Model.test.ts
+++ packages/blvd/__tests__/model/Model.test.ts
@@ -1,4 +1,5 @@
 import { Model, PropertyTypes } from '../../src'
+import { ModelPropertiesObject } from '../../src/model/Model'
 
 class Car extends Model {
   public static propertyTypes: object = {
@@ -8,19 +9,36 @@ class Car extends Model {
     inStock: PropertyTypes.boolean
   }
 
-  constructor (...props) {
-    super(...props)
+  constructor (props: ModelPropertiesObject) {
+    super(props)
   }
 }
 
-const lamborghiniHuracan = Car.make({
-  name: 'Lamborghini Huracan',
-  year: 2014,
-  inStock: true
-})
-
 describe('Model', () => {
   it('creates the model properly', () => {
-    expect(true).toBe(true)
+    let lamborghiniHuracan: Car
+
+    Car.make({
+      name: 'Lamborghini Huracán',
+      year: 2014,
+      inStock: true
+    }).then((res: Car) => {
+      lamborghiniHuracan = res
+    }).catch((err: any) => { console.error(err) })
+    expect(lamborghiniHuracan).toBeInstanceOf(Car)
+  })
+  it('has the correct properties', () => {
+    let lamborghiniHuracan: Car
+
+    Car.make({
+      name: 'Lamborghini Huracán',
+      year: 2014,
+      inStock: true
+    }).then((res: Car) => {
+      lamborghiniHuracan = res
+    }).catch((err: any) => { console.error(err) })
+    expect(lamborghiniHuracan.properties.name).toBe('Lamborghini Huracán')
+    expect(lamborghiniHuracan.properties.year).toBe(2014)
+    expect(lamborghiniHuracan.properties.inStock).toBe(true)
   })
 })
