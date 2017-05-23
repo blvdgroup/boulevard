951437bea93c9cecac2489d4f9e08c4e043debab
Resi Respati <resir014@gmail.com>
2017-05-24 00:23:29 +0700

    fix(blvd:ModelConstructor): updated ModelConstructor parameters

--- packages/blvd/src/model/ModelConstructor.ts
+++ packages/blvd/src/model/ModelConstructor.ts
@@ -1,10 +1,10 @@
-import Model from './Model'
+import Model, { ModelPropertiesObject } from './Model'
 
 interface ModelConstructor {
   propertyTypes: object,
-  new(properties?: object, ianctd?: boolean): Model,
-  make(): Promise<Model>,
-  getById(id: string): Promise<Model>
+  new(properties: ModelPropertiesObject, ianctd?: boolean): Model,
+  make(properties: ModelPropertiesObject): Promise<Model>,
+  // getById(id: string): Promise<Model>
 }
 
 export default ModelConstructor
