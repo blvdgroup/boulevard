fcdbf433b4f2937b40bf47c49a18045b90b70643
Kyle <kyle@vulpine.me>
2017-05-26 23:55:51 -0400

    feat(blvd:PropertyTypes): change property type pending change where we check prop types earlier

--- packages/blvd/src/propertyTypes/PropertyType.ts
+++ packages/blvd/src/propertyTypes/PropertyType.ts
@@ -1,8 +1,5 @@
-import Model from '../model/Model'
-import ModelConstructor from '../model/ModelConstructor'
-
 import { Result } from 'blvd-utils'
 
-type PropertyType = (property: any, item?: Model, model?: ModelConstructor) => (Result | Promise<Result>)
+type PropertyType = (property: any, key: string) => (Result | Promise<Result>)
 
 export default PropertyType
--- packages/blvd/src/propertyTypes/PropertyTypes.ts
+++ packages/blvd/src/propertyTypes/PropertyTypes.ts
@@ -2,13 +2,24 @@ import { Status } from 'blvd-utils'
 
 import PropertyType from './PropertyType'
 
+const prefix = 'I tried to create a new model, but one of the properties was of the incorrect type.'
+
 /**
  * I wrote a bunch of stuff here but it's aaaall very wrong now so I have deleted what was written here.
  */
 class PropertyTypes {
-  public static string: PropertyType = (o: any) => typeof o === 'string' ? { status: Status.SUCCESS } : { status: Status.FAILURE }
-  public static number: PropertyType = (o: any) => typeof o === 'number' ? { status: Status.SUCCESS } : { status: Status.FAILURE }
-  public static boolean: PropertyType = (o: any) => typeof o === 'boolean' ? { status: Status.SUCCESS } : { status: Status.FAILURE }
+  public static string: PropertyType = (o: any, k: string) => typeof o === 'string'
+    ? { status: Status.SUCCESS }
+    : { status: Status.FAILURE, error: prefix + `${k} was supposed to be a string, but was a ${typeof o} instead.` }
+  public static number: PropertyType = (o: any, k: string) => typeof o === 'number'
+    ? { status: Status.SUCCESS }
+    : { status: Status.FAILURE, error: prefix + `${k} was supposed to be a number, but was a ${typeof o} instead.` }
+  public static boolean: PropertyType = (o: any, k: string) => typeof o === 'boolean'
+    ? { status: Status.SUCCESS }
+    : { status: Status.FAILURE, error: prefix + `${k} was supposed to be a boolean, but was a ${typeof o} instead.` }
+  public static required: PropertyType = (o: any, k: string) => typeof o !== 'undefined'
+    ? { status: Status.SUCCESS }
+    : { status: Status.FAILURE, error: prefix + `${k} was supposed to be defined, but wasn't.` }
 }
 
 export default PropertyTypes
